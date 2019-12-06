c53a01e483fb7134fa7dae0ec2a8c13c7920c3e4
unknown <patddong@naver.com>
2019-12-07 01:18:32 +0900

    third ver.

--- final_pokemon/func.c
+++ final_pokemon/func.c
@@ -58,6 +58,30 @@ void moving(int huntfield[4][4])
 		}
 }
 
+void moving_village(int village[3][3])
+{
+	int y=0;
+	int x=0;
+	int SIZE=1;
+		while (village[x][y]==0) {
+			x=getX();
+			y=getY();
+			if (GetAsyncKeyState(VK_UP)) {
+				//상점
+			}
+			if (GetAsyncKeyState(VK_DOWN)) {
+				//포켓센터
+			}
+			if (GetAsyncKeyState(VK_RIGHT)) {
+				//체육관
+			}
+			if (GetAsyncKeyState(VK_LEFT)) {
+				//사냥터 진입
+			}
+			gotoxy(x, y);
+			Sleep(500);
+		}
+}
 
 //플레이어 공간생성
 Player *make_player(Player *player) {
@@ -82,7 +106,7 @@ Player *make_player(Player *player) {
 
 //포켓몬 공간 생성
 Pokemon* make_pokemon(Pokemon* pokemon) {
-	pokemon = (Pokemon*) malloc(sizeof(Pokemon) * 1);
+	pokemon = (Pokemon*) malloc(sizeof(Pokemon) * 6);
 	if (pokemon == NULL) {
 		printf("포켓몬공간 할당실패");
 	}
@@ -228,7 +252,30 @@ void print_player(Player *player) {
 //Pokemon **make_pokemon_bag_col(Pokemon ***bag_addr) {
 //
 //}
+//void make_pokemon_room(Player *p1)
+//{
+//	p1->pokemon=(Pokemon*)malloc(sizeof(Pokemon)*6);
+//}
 
+void select_pokemon(Player *p1)
+{
+	int i=0;
+	int j=0;
+	int k=1;
+	int a=0;
+	printf("당신의 현재 포켓몬!\n");
+
+	for(j=0;j<4;j++){
+	for(i=0;i<_msize(p1->pokemon_bag+j)/sizeof(p1->pokemon_bag+j);i++)
+	{
+		printf("%d . 이름 :%s 공격력 : %d 체력 : %d 최대체력 :%d",k,(*(p1->pokemon_bag+i)+j)->name,(*(p1->pokemon_bag+i)+j)->power,(*(p1->pokemon_bag+i)+j)->max_hp);
+		k++;	
+	}
+	}
+	printf("몇번 포켓몬을 넣으시겠습니까? (최대 6마리)");
+	scanf("%d",&a);		
+
+}
 
 int check_pokemon_hp(Player p1,int n)	//포켓몬이 한마리라도 살아있으면 1, 다 죽었으면 2를 리턴.
 {
@@ -239,34 +286,34 @@ int check_pokemon_hp(Player p1,int n)	//포켓몬이 한마리라도 살아있으면 1, 다 죽�
 	return 2;
 }
 
-void battle_gym(Player p1,Player *boss)
+void battle_gym(Player *p1,Player *boss)
 {
 	int i,j;
 	int random;
 	int p1_win_or_lose;
 	int boss_win_or_lose;
-
+	Player boss_temp;		//플레이어의 뱃지 개수 기준
 	srand((unsigned)time(NULL));
 	printf("싸움을 시작합니다.\n");
 	while(p1_win_or_lose==1||boss_win_or_lose==1)	{
-	while(p1.pokemon[j].hp>0){
+	while(p1->pokemon[j].hp>0){
 	printf("행동을 선택하세요 1. 평타  2. 스킬  3.가방\n");
 	scanf("%d",&i);
 	if(i==1){
-		boss->pokemon->hp-=p1.pokemon->power;
+		boss->pokemon->hp-=p1->pokemon->power;
 	}
 	if(i==2){
-		boss->pokemon->hp-=p1.pokemon->skill(&p1,boss);//스킬함수에 power 배수를 해줘야 합니다.
+		boss->pokemon->hp-=p1->pokemon->skill(&p1,boss);//스킬함수에 power 배수를 해줘야 합니다.
 	}
 	if(i==3){
-		printf("쓸 아이템 : 1.포켓볼 %d 개 2.상처약 %d 개",p1.inventory->monster_ball,p1.inventory->medicine);
+		printf("쓸 아이템 : 1.포켓볼 %d 개 2.상처약 %d 개",p1->inventory->monster_ball,p1->inventory->medicine);
 		
 	printf("적이 공격합니다.\n");
 	random = rand()%2+1;
 	if(random==1)
-		p1.pokemon->hp-=boss->pokemon->power;
+		p1->pokemon->hp-=boss->pokemon->power;
 	if(i==2)
-		p1.pokemon->hp-=boss->pokemon->skill(&p1,boss);
+		p1->pokemon->hp-=boss->pokemon->skill(&p1,boss);
 	}
 		p1_win_or_lose=check_pokemon_hp(p1,_msize(p1.pokemon_bag)/sizeof(p1.pokemon_bag));//정수 부분은 현재 플레이어가 가지고 있는 포켓몬의 갯수를 넣어야 함.
 		boss_win_or_lose=check_pokemon_hp(boss[1],_msize(boss[1].pokemon_bag)/sizeof(boss[1].pokemon_bag));
@@ -281,7 +328,7 @@ void battle_gym(Player p1,Player *boss)
 	if(boss_win_or_lose==2)
 		{
 			printf("승리!");
-			p1.badge+=1;
+			p1->badge+=1;
 			return;
 		}
 
@@ -302,12 +349,12 @@ int searching_monster(int hunt_field[4][4]){
 
 
 
-int monster_ball_fail_or_success(Pokemon temp_pokemon)
+int monster_ball_fail_or_success(Pokemon *temp_pokemon)
 {
 	int percentage=0;
 	int a;
 	float i;
-	a=100*(float)temp_pokemon.hp/(float)temp_pokemon.max_hp;
+	a=100*(float)temp_pokemon->hp/(float)temp_pokemon->max_hp;
 		percentage=rand()%(100-a) + (1+a);
 		i=percentage/100;
 	if(i>0.7) return 1;
@@ -315,17 +362,17 @@ int monster_ball_fail_or_success(Pokemon temp_pokemon)
 }
 
 
-void catch_pokemon(Player p1,Pokemon *temp_pokemon)
-{
+//void catch_pokemon(Player* p1,Pokemon *temp_pokemon)		//이것을 계속해서 1, 2, 3 늘려가야 하는데, 여기서 realloc 함수가 필요합니다.
+//{
 	
 		//잡았을 경우 catch 함수
-		p1.pokemon_bag[1]->name=pokemon_dic.name;
-		p1.pokemon_bag[1]->power=pokemon_dic.power;
-		p1.pokemon_bag[1]->hp=pokemon_dic.hp;
-		p1.pokemon_bag[1]->max_hp=pokemon_dic.max_hp;
-		p1.pokemon_bag[1]->skill=pokemon_dic.skill;
+//		p1->pokemon_bag[1]->name=temp_pokemon->name;
+//		p1->pokemon_bag[1]->power=temp_pokemon->power;
+//		p1->pokemon_bag[1]->hp=temp_pokemon->hp;
+//		p1->pokemon_bag[1]->max_hp=temp_pokemon->max_hp;
+//		p1->pokemon_bag[1]->skill=temp_pokemon->skill;
 
-}
+//
 
 rand_pokemon *random_pokemon(Pokemon **pokemon_dic,Pokemon *temp_pokemon)
 {
@@ -343,7 +390,80 @@ rand_pokemon *random_pokemon(Pokemon **pokemon_dic,Pokemon *temp_pokemon)
 	return a;
 }
 
-void fighting();
+void using_fist(Player *p1,Pokemon temp_pokemon)
+{
+	
+	
+
+}
+
+void using_skill(Player *p1,Pokemon temp_pokemon)
+{
+
+}
+
+void healing(Player *p1)
+{
+	int i;
+
+	printf("현재 체력 : %d",(p1->pokemon+i)->hp);
+	if(((p1->pokemon+i)->hp)+50>=(p1->pokemon+i)->max_hp)
+		((p1->pokemon+i)->hp)=((p1->pokemon+i)->max_hp);
+	else
+		((p1->pokemon+i)->hp)+=50;
+}
+	
+
+
+void using_backpack(Player *p1,Pokemon *temp_pokemon)
+{
+	int choosen;
+	int success_or_fail;
+	printf("1.포켓볼 %d 개  2.상처약 %d 개  3. 진화의 돌 %d 개",p1->inventory->monster_ball,p1->inventory->medicine,p1->inventory->evolution_stone);
+	scanf("%d",&choosen);
+
+	switch(choosen)
+	{
+	case 1:
+		p1->inventory->monster_ball-=1;
+		success_or_fail=monster_ball_fail_or_success(temp_pokemon);
+		if(success_or_fail==1)
+			catch_pokemon(p1,temp_pokemon);
+			break;
+	case 2:
+		p1->inventory->medicine-=1;
+		printf("치료를 합니다,\n");
+		healing(p1);
+		break;
+	case 3:
+		printf("사용이 불가능 합니다,");
+		break;
+}
+}
+}
+
+void fighting(Player *p1,Pokemon temp_pokemon)
+{
+	int manu;
+	while(1)	{
+	printf("어떤 행동을 하시겠습니까?\n");
+	printf("1.직접 공격 2.스킬 3.도구사용");
+	scanf("%d",&manu);
+	switch(manu)	{
+	case 1:
+		using_fist(p1,temp_pokemon);
+		continue;
+	case 2:
+		using_skill(p1,temp_pokemon);
+			continue;
+	case 3:
+		using_backpack(p1,temp_pokemon);
+		continue;
+	}
+	}
+}
+
+
 
 void hunt(Player p1,int hunt_field[4][4],Pokemon **pokemon_dic)
 {
